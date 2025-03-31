11da1c7cdca494401671ec5644bace72f77a4f70
Anderson Lucas <114498251+andersoncsgo@users.noreply.github.com>
2025-03-31 12:46:38 -0300

    1 commit

--- poo.py
+++ poo.py
@@ -5,4 +5,16 @@ class Pessoa:
     def __init__(self, nome, idade):
         self.nome = nome
         self.idade = idade
+
+    def saudacao(self):
+        return f"Ola, meu nome e {self.nome} e eu tenho {self.idade} anos."
         
+# Obejetos
+pessoa1 = Pessoa("Alice", 30)
+print("Nome:", pessoa1.nome)
+mensagem = pessoa1.saudacao()
+print(mensagem)
+
+pessoa2 = Pessoa(nome="Rodrigo", idade="32")
+mensagem = pessoa2.saudacao()
+print(mensagem)
